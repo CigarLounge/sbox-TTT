e17dc5e2c36a0ea0c16f0174cffb7676ed206922
Matthew Zegar <matthewzegar@gmail.com>
2022-01-26 20:54:48 -0800

    Remove editor config

--- code/items/weapons/Knife.cs
+++ code/items/weapons/Knife.cs
@@ -8,186 +8,186 @@ using TTT.UI;
 
 namespace TTT.Items
 {
-    [Library("weapon_knife")]
-    [Weapon(SlotType = SlotType.Melee)]
-    [Buyable(Price = 100)]
-    [Precached("weapons/swb/hands/swat/v_hands_swat.vmdl", "weapons/swb/melee/bayonet/v_bayonet.vmdl", "weapons/swb/melee/bayonet/w_bayonet.vmdl")]
-    [Hammer.EditorModel("weapons/swb/melee/bayonet/w_bayonet.vmdl")]
-    public class Knife : TTTWeaponBaseMelee
-    {
-        public override int Bucket => 0;
-        public override HoldType HoldType => HoldType.Fists; // just use fists for now
-        public override string HandsModelPath => "weapons/swb/hands/swat/v_hands_swat.vmdl";
-        public override string ViewModelPath => "weapons/swb/melee/bayonet/v_bayonet.vmdl";
-        public override AngPos ViewModelOffset => new()
-        {
-            Angle = new Angles(0, -15, 0),
-            Pos = new Vector3(-4, 0, 0)
-        };
-        public override string WorldModelPath => "weapons/swb/melee/bayonet/w_bayonet.vmdl";
-        public override string Icon => "/swb_weapons/textures/bayonet.png";
-        public override int FOV => 75;
-        public override float WalkAnimationSpeedMod => 1.25f;
+	[Library( "weapon_knife" )]
+	[Weapon( SlotType = SlotType.Melee )]
+	[Buyable( Price = 100 )]
+	[Precached( "weapons/swb/hands/swat/v_hands_swat.vmdl", "weapons/swb/melee/bayonet/v_bayonet.vmdl", "weapons/swb/melee/bayonet/w_bayonet.vmdl" )]
+	[Hammer.EditorModel( "weapons/swb/melee/bayonet/w_bayonet.vmdl" )]
+	public class Knife : TTTWeaponBaseMelee
+	{
+		public override int Bucket => 0;
+		public override HoldType HoldType => HoldType.Fists; // just use fists for now
+		public override string HandsModelPath => "weapons/swb/hands/swat/v_hands_swat.vmdl";
+		public override string ViewModelPath => "weapons/swb/melee/bayonet/v_bayonet.vmdl";
+		public override AngPos ViewModelOffset => new()
+		{
+			Angle = new Angles( 0, -15, 0 ),
+			Pos = new Vector3( -4, 0, 0 )
+		};
+		public override string WorldModelPath => "weapons/swb/melee/bayonet/w_bayonet.vmdl";
+		public override string Icon => "/swb_weapons/textures/bayonet.png";
+		public override int FOV => 75;
+		public override float WalkAnimationSpeedMod => 1.25f;
 
-        public override string SwingAnimationHit => "stab";
-        public override string SwingAnimationMiss => "stab_miss";
-        public override string StabAnimationHit => "stab";
-        public override string StabAnimationMiss => "stab_miss";
-        public override string SwingSound => "bayonet.stab";
-        public override string StabSound => "bayonet.stab";
-        public override string MissSound => "bayonet.slash";
-        public override string HitWorldSound => "bayonet.hitwall";
-        public override float SwingSpeed => 1f;
-        public override float StabSpeed => 1f;
-        public override float SwingDamage => 200f;
-        public override float StabDamage => 50f;
-        public override float SwingForce => 25f;
-        public override float StabForce => 50f;
-        public override float DamageDistance => 35f;
-        public override float ImpactSize => 10f;
+		public override string SwingAnimationHit => "stab";
+		public override string SwingAnimationMiss => "stab_miss";
+		public override string StabAnimationHit => "stab";
+		public override string StabAnimationMiss => "stab_miss";
+		public override string SwingSound => "bayonet.stab";
+		public override string StabSound => "bayonet.stab";
+		public override string MissSound => "bayonet.slash";
+		public override string HitWorldSound => "bayonet.hitwall";
+		public override float SwingSpeed => 1f;
+		public override float StabSpeed => 1f;
+		public override float SwingDamage => 200f;
+		public override float StabDamage => 50f;
+		public override float SwingForce => 25f;
+		public override float StabForce => 50f;
+		public override float DamageDistance => 35f;
+		public override float ImpactSize => 10f;
 
-        private readonly float _primaryEntitySpeed = 200f;
-        private Vector3 _entityVelocity = new(0, 0, 10);
-        private Vector3 _entitySpawnOffset = new(0, 10, 50);
-        private Angles _entityAngles = new(90, -60, 10);
+		private readonly float _primaryEntitySpeed = 200f;
+		private Vector3 _entityVelocity = new( 0, 0, 10 );
+		private Vector3 _entitySpawnOffset = new( 0, 10, 50 );
+		private Angles _entityAngles = new( 90, -60, 10 );
 
-        public Knife()
-        {
+		public Knife()
+		{
 
-        }
+		}
 
-        public override void MeleeAttack(float damage, float force, string hitAnimation, string missAnimation, string sound)
-        {
-            TimeSincePrimaryAttack = 0;
-            TimeSinceSecondaryAttack = 0;
+		public override void MeleeAttack( float damage, float force, string hitAnimation, string missAnimation, string sound )
+		{
+			TimeSincePrimaryAttack = 0;
+			TimeSinceSecondaryAttack = 0;
 
-            var hitEntity = true;
-            var pos = Owner.EyePos;
-            var forward = Owner.EyeRot.Forward;
-            var trace = Trace.Ray(pos, pos + forward * DamageDistance)
-                .Ignore(this)
-                .Ignore(Owner)
-                .Size(ImpactSize)
-                .Run();
+			var hitEntity = true;
+			var pos = Owner.EyePos;
+			var forward = Owner.EyeRot.Forward;
+			var trace = Trace.Ray( pos, pos + forward * DamageDistance )
+				.Ignore( this )
+				.Ignore( Owner )
+				.Size( ImpactSize )
+				.Run();
 
-            if (!trace.Entity.IsValid() || trace.Entity.IsWorld)
-            {
-                hitAnimation = missAnimation;
-                sound = !trace.Entity.IsValid() ? MissSound : HitWorldSound;
-                hitEntity = false;
-            }
+			if ( !trace.Entity.IsValid() || trace.Entity.IsWorld )
+			{
+				hitAnimation = missAnimation;
+				sound = !trace.Entity.IsValid() ? MissSound : HitWorldSound;
+				hitEntity = false;
+			}
 
-            DoMeleeEffects(hitAnimation, sound);
-            (Owner as AnimEntity).SetAnimBool("b_attack", true);
+			DoMeleeEffects( hitAnimation, sound );
+			(Owner as AnimEntity).SetAnimBool( "b_attack", true );
 
-            if (!hitEntity || !IsServer) return;
+			if ( !hitEntity || !IsServer ) return;
 
-            using (Prediction.Off())
-            {
-                var damageInfo = DamageInfo.FromBullet(trace.EndPos, forward * force, damage)
-                    .UsingTraceResult(trace)
-                    .WithAttacker(Owner)
-                    .WithWeapon(this);
+			using ( Prediction.Off() )
+			{
+				var damageInfo = DamageInfo.FromBullet( trace.EndPos, forward * force, damage )
+					.UsingTraceResult( trace )
+					.WithAttacker( Owner )
+					.WithWeapon( this );
 
-                trace.Entity.TakeDamage(damageInfo);
+				trace.Entity.TakeDamage( damageInfo );
 
-                if (trace.Entity is TTTPlayer)
-                {
-                    Owner.Inventory.Drop(this);
-                    Delete();
-                }
-            }
-        }
+				if ( trace.Entity is TTTPlayer )
+				{
+					Owner.Inventory.Drop( this );
+					Delete();
+				}
+			}
+		}
 
-        public override void AttackSecondary()
-        {
-            if (Host.IsServer)
-            {
-                using (Prediction.Off())
-                {
-                    ThrowKnife(new ThrownKnife());
-                    Owner.Inventory.Drop(this);
-                    Delete();
-                }
-            }
-        }
+		public override void AttackSecondary()
+		{
+			if ( Host.IsServer )
+			{
+				using ( Prediction.Off() )
+				{
+					ThrowKnife( new ThrownKnife() );
+					Owner.Inventory.Drop( this );
+					Delete();
+				}
+			}
+		}
 
-        public void ThrowKnife(ThrownKnife knife)
-        {
-            if (!string.IsNullOrEmpty(WorldModelPath))
-                knife.SetModel(WorldModelPath);
+		public void ThrowKnife( ThrownKnife knife )
+		{
+			if ( !string.IsNullOrEmpty( WorldModelPath ) )
+				knife.SetModel( WorldModelPath );
 
-            knife.Owner = Owner;
-            knife.Position = MathUtil.RelativeAdd(Position, _entitySpawnOffset, Owner.EyeRot);
-            knife.Rotation = Owner.EyeRot * Rotation.From(_entityAngles);
-            knife.RemoveDelay = -1;
-            knife.UseGravity = true;
-            knife.Speed = _primaryEntitySpeed;
-            knife.IsSticky = true;
-            knife.Damage = SwingDamage;
-            knife.Force = Primary.Force;
-            knife.StartVelocity = MathUtil.RelativeAdd(Vector3.Zero, _entityVelocity, Owner.EyeRot);
-            knife.Start();
-        }
+			knife.Owner = Owner;
+			knife.Position = MathUtil.RelativeAdd( Position, _entitySpawnOffset, Owner.EyeRot );
+			knife.Rotation = Owner.EyeRot * Rotation.From( _entityAngles );
+			knife.RemoveDelay = -1;
+			knife.UseGravity = true;
+			knife.Speed = _primaryEntitySpeed;
+			knife.IsSticky = true;
+			knife.Damage = SwingDamage;
+			knife.Force = Primary.Force;
+			knife.StartVelocity = MathUtil.RelativeAdd( Vector3.Zero, _entityVelocity, Owner.EyeRot );
+			knife.Start();
+		}
 
-        public class ThrownKnife : FiredEntity, IEntityHint
-        {
-            public float HintDistance => 80f;
+		public class ThrownKnife : FiredEntity, IEntityHint
+		{
+			public float HintDistance => 80f;
 
-            public TranslationData TextOnTick => TTTWeaponBaseGeneric.PickupText("weapon_knife");
+			public TranslationData TextOnTick => TTTWeaponBaseGeneric.PickupText( "weapon_knife" );
 
-            public bool CanHint(TTTPlayer client)
-            {
-                return true;
-            }
+			public bool CanHint( TTTPlayer client )
+			{
+				return true;
+			}
 
-            public EntityHintPanel DisplayHint(TTTPlayer client)
-            {
-                return new Hint(TextOnTick);
-            }
+			public EntityHintPanel DisplayHint( TTTPlayer client )
+			{
+				return new Hint( TextOnTick );
+			}
 
-            private bool _hasLanded = false;
+			private bool _hasLanded = false;
 
-            public void Tick(TTTPlayer player)
-            {
-                if (Host.IsClient)
-                {
-                    return;
-                }
+			public void Tick( TTTPlayer player )
+			{
+				if ( Host.IsClient )
+				{
+					return;
+				}
 
-                if (player.LifeState != LifeState.Alive)
-                {
-                    return;
-                }
+				if ( player.LifeState != LifeState.Alive )
+				{
+					return;
+				}
 
-                using (Prediction.Off())
-                {
-                    if (Input.Pressed(InputButton.Use))
-                    {
-                        player.Inventory.TryAdd(new Knife(), deleteIfFails: false, makeActive: true);
-                        Delete();
-                    }
-                }
-            }
+				using ( Prediction.Off() )
+				{
+					if ( Input.Pressed( InputButton.Use ) )
+					{
+						player.Inventory.TryAdd( new Knife(), deleteIfFails: false, makeActive: true );
+						Delete();
+					}
+				}
+			}
 
-            protected override void OnPhysicsCollision(CollisionEventData eventData)
-            {
-                if (!_hasLanded && eventData.Entity is TTTPlayer playerHit)
-                {
-                    DamageInfo info = new()
-                    {
-                        Damage = Damage,
-                        Force = 50f,
-                        Attacker = Owner,
-                        Weapon = this
-                    };
+			protected override void OnPhysicsCollision( CollisionEventData eventData )
+			{
+				if ( !_hasLanded && eventData.Entity is TTTPlayer playerHit )
+				{
+					DamageInfo info = new()
+					{
+						Damage = Damage,
+						Force = 50f,
+						Attacker = Owner,
+						Weapon = this
+					};
 
-                    playerHit.TakeDamage(info);
-                    Delete();
-                }
+					playerHit.TakeDamage( info );
+					Delete();
+				}
 
-                _hasLanded = true;
-            }
-        }
-    }
+				_hasLanded = true;
+			}
+		}
+	}
 }
