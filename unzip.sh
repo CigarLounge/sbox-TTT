934bdc6e896d2541f99cdd7e74354be4327ec18e
Matthew Zegar <matthewzegar@gmail.com>
2022-02-07 20:06:49 -0800

    Update git ignore

--- unzip.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-git submodule update --recursive --remote
-cp -a ./ttt-models/materials ./
-cp -a ./ttt-models/models ./
-cp -a ./ttt-models/sounds ./
--- zip.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-cp -a ./materials ./ttt-models/
-cp -a ./models ./ttt-models/
-cp -a ./sounds ./ttt-models/
