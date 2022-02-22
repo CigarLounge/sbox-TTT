7e3beed5907e7d7774689d286a4a608a71189fe7
Matthew Zegar <matthewzegar@gmail.com>
2022-02-22 14:01:20 -0800

    Use actual asset colours

--- code/roles/BaseRole.cs
+++ code/roles/BaseRole.cs
@@ -9,7 +9,7 @@ namespace TTT;
 public partial class RoleInfo : Asset
 {
 	[Property] public Team Team { get; set; } = Team.None;
-	[Property] public Color Color { get; set; } // https://github.com/Facepunch/sbox-issues/issues/928
+	[Property] public Color Color { get; set; }
 	[Property] public int DefaultCredits { get; set; }
 	[Property] public List<string> ExclusiveItems { get; set; } // It'd be cool if s&box let us select `Assets` here.
 
@@ -18,17 +18,6 @@ public partial class RoleInfo : Asset
 	protected override void PostLoad()
 	{
 		base.PostLoad();
-
-		// Blame s&box
-		// https://github.com/Facepunch/sbox-issues/issues/928
-		if ( LibraryName == "ttt_role_none" )
-			Color = Color.Transparent;
-		else if ( LibraryName == "ttt_role_traitor" )
-			Color = Color.FromBytes( 223, 41, 53 );
-		else if ( LibraryName == "ttt_role_innocent" )
-			Color = Color.FromBytes( 27, 197, 78 );
-		else if ( LibraryName == "ttt_role_detective" )
-			Color = Color.FromBytes( 25, 102, 255 );
 
 		AvailableItems = ExclusiveItems == null ? new HashSet<string>() : new HashSet<string>( ExclusiveItems );
 	}
