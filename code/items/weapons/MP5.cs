00cfce1281082514dcc845ad0c53848fe703d445
Matthew Zegar <matthewzegar@gmail.com>
2022-02-21 20:06:15 -0800

    Remove spawnable tag

--- code/items/weapons/MP5.cs
+++ code/items/weapons/MP5.cs
@@ -3,7 +3,7 @@
 namespace TTT;
 
 [Hammer.EditorModel( "weapons/rust_pistol/rust_pistol.vmdl" )]
-[Library( "ttt_weapon_mp5", Title = "MP5", Spawnable = true )]
+[Library( "ttt_weapon_mp5", Title = "MP5" )]
 public partial class MP5 : Weapon
 {
 }
--- code/items/weapons/P250.cs
+++ code/items/weapons/P250.cs
@@ -3,7 +3,7 @@
 namespace TTT;
 
 [Hammer.EditorModel( "weapons/rust_pistol/rust_pistol.vmdl" )]
-[Library( "ttt_weapon_p250", Title = "P250", Spawnable = true )]
+[Library( "ttt_weapon_p250", Title = "P250" )]
 public partial class P250 : Weapon
 {
 }
