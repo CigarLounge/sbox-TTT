6cb3b543db0646c8ca447b668a61ac970d3dff61
Matthew Zegar <matthewzegar@gmail.com>
2022-02-23 00:10:26 -0800

    Add revolver + hk417

---
+++ code/items/weapons/Revolver.cs
@@ -0,0 +1,9 @@
+using Sandbox;
+
+namespace TTT;
+
+[Hammer.EditorModel( "models/weapons/w_revolver.wmdl" )]
+[Library( "ttt_weapon_revolver", Title = "Revolver" )]
+public partial class Revolver : Weapon
+{
+}
