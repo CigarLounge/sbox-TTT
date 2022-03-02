7f8ba02ebe3ffe3e23024a51d1a207a53cabaca1
Matthew Zegar <matthewzegar@gmail.com>
2022-03-01 16:16:37 -0800

    Move WIP indicator, change info colour

--- code/ui/general/wipdisclaimer/WIPDisclaimer.cs
+++ code/ui/general/wipdisclaimer/WIPDisclaimer.cs
@@ -14,13 +14,12 @@ public class WIPDisclaimer : Panel
 		Panel wrapper = new( this );
 		wrapper.Style.FlexDirection = FlexDirection.Column;
 		wrapper.Style.TextAlign = TextAlign.Center;
-		wrapper.AddClass( "centered-vertical-10" );
+		wrapper.AddClass( "centered-vertical-90" );
 		wrapper.AddClass( "opacity-medium" );
 		wrapper.AddClass( "text-color-info" );
 		wrapper.AddClass( "text-shadow" );
 
 		wrapper.Add.Label( "TTT is work-in-progress!" );
-		wrapper.Add.Label( "Everything you see is subject to change or is actively being worked on!" );
 		wrapper.Add.Label( "Our project is open source, consider contributing at github.com/mzegar/sbox-TTT" );
 
 		AddClass( "fullscreen" );
