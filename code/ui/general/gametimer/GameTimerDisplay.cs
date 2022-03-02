2f1bcbb0a65cfa030aa510e57a8d4abf3d8a1127
Matthew Zegar <matthewzegar@gmail.com>
2022-03-01 18:01:25 -0800

    Fea: New round timer

--- code/ui/general/gametimer/GameTimerDisplay.cs
+++ code/ui/general/gametimer/GameTimerDisplay.cs
@@ -1,43 +1,44 @@
-using Sandbox.UI;
-using Sandbox.UI.Construct;
+// using Sandbox.UI;
+// using Sandbox.UI.Construct;
 
-namespace TTT.UI;
+// namespace TTT.UI;
 
-public class GameTimerDisplay : Panel
-{
-	private readonly Label _timerLabel;
-	private readonly Label _roundLabel;
+// public class GameTimerDisplay : Panel
+// {
+// 	private readonly Label _timerLabel;
+// 	private readonly Label _roundLabel;
 
-	public GameTimerDisplay()
-	{
-		StyleSheet.Load( "/ui/general/gametimer/GameTimerDisplay.scss" );
+// 	public GameTimerDisplay()
+// 	{
+// 		StyleSheet.Load( "/ui/general/gametimer/GameTimerDisplay.scss" );
 
-		AddClass( "background-color-primary" );
-		AddClass( "centered-horizontal" );
-		AddClass( "opacity-heavy" );
-		AddClass( "rounded" );
-		AddClass( "text-shadow" );
+// 		AddClass( "background-color-primary" );
+// 		AddClass( "centered-horizontal" );
+// 		AddClass( "opacity-heavy" );
+// 		AddClass( "rounded" );
+// 		AddClass( "text-shadow" );
 
-		_roundLabel = Add.Label();
-		_roundLabel.AddClass( "round-label" );
-		_roundLabel.AddClass( "text-color-info" );
+// 		_roundLabel = Add.Label();
+// 		_roundLabel.AddClass( "round-label" );
+// 		_roundLabel.AddClass( "text-color-info" );
 
-		_timerLabel = Add.Label();
-		_timerLabel.AddClass( "timer-label" );
-	}
+// 		_timerLabel = Add.Label();
+// 		_timerLabel.AddClass( "timer-label" );
+// 	}
 
-	public override void Tick()
-	{
-		base.Tick();
+// 	public override void Tick()
+// 	{
+// 		base.Tick();
 
-		if ( Game.Current.Round == null )
-		{
-			return;
-		}
+// 		if ( Game.Current.Round == null )
+// 		{
+// 			return;
+// 		}
 
-		_roundLabel.Text = $"{Game.Current.Round.RoundName.ToUpper()}";
+// 		_roundLabel.Text = $"{Game.Current.Round.RoundName.ToUpper()}";
 
-		_timerLabel.Enabled( Game.Current.Round is not WaitingRound );
-		_timerLabel.Text = Game.Current.Round.TimeLeftFormatted;
-	}
-}
+// 		_timerLabel.Enabled( Game.Current.Round is not WaitingRound );
+// 		_timerLabel.Text = Game.Current.Round.TimeLeftFormatted;
+// 	}
+// }
+
