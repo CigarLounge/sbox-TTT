68d3ea7f5c3a27e2a1371f9b012b0d8ba27dbf5f
Matthew Zegar <matthewzegar@gmail.com>
2022-03-02 21:18:44 -0800

    Remove credits from PlayerInfo

---
+++ code/ui/player/playerinfo/PlayerInfo.cs
@@ -0,0 +1,59 @@
+using Sandbox;
+using Sandbox.UI;
+using Sandbox.UI.Construct;
+
+namespace TTT.UI;
+
+public class PlayerInfo : Panel
+{
+	private Panel _roleContainer;
+	private Label _role;
+
+	private Panel _healthContainer;
+	private Label _health;
+
+	public PlayerInfo()
+	{
+		StyleSheet.Load( "/ui/player/playerinfo/PlayerInfo.scss" );
+
+		AddClass( "opacity-heavy" );
+		AddClass( "text-shadow" );
+
+		_roleContainer = Add.Panel( "role-container" );
+		_role = _roleContainer.Add.Label();
+
+		_healthContainer = Add.Panel( "health-container background-color-primary" );
+		_health = _healthContainer.Add.Label();
+	}
+
+	public override void Tick()
+	{
+		if ( Local.Pawn is not Player player )
+		{
+			this.Enabled( false );
+			return;
+		}
+
+		this.Enabled( player.CurrentPlayer.IsAlive() );
+		if ( !this.IsEnabled() )
+			return;
+
+		if ( player.CurrentPlayer.Role is NoneRole )
+		{
+			_roleContainer.Enabled( false );
+			_healthContainer.Style.BorderTopLeftRadius = 8;
+			_healthContainer.Style.BorderTopRightRadius = 8;
+		}
+		else
+		{
+			_roleContainer.Enabled( true );
+			_roleContainer.Style.BackgroundColor = player.CurrentPlayer.Role.Info.Color;
+			_role.Text = player.CurrentPlayer.Role.Info.Title;
+
+			_healthContainer.Style.BorderTopLeftRadius = 0;
+			_healthContainer.Style.BorderTopRightRadius = 0;
+		}
+
+		_health.Text = $"✚ {player.CurrentPlayer.Health.CeilToInt()}";
+	}
+}
