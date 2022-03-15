31ef314e9b31ec0c8b6634ea4010b11b7247b25a
Matthew Zegar <matthewzegar@gmail.com>
2022-03-14 18:17:07 -0700

    General cleanup

--- code/items/weapons/HK417.cs
+++
@@ -1,9 +0,0 @@
-using Sandbox;
-
-namespace TTT;
-
-[Hammer.EditorModel( "models/weapons/w_hk417.vmdl" )]
-[Library( "ttt_weapon_hk417", Title = "HK417" )]
-public partial class HK417 : Weapon
-{
-}
