11b41bee1ebf3672bbf8ba369e8f045ec342d6b6
Matthew Zegar <matthewzegar@gmail.com>
2022-03-16 00:09:26 -0700

    Basic radio setup

--- code/ui/player/rolemenu/radio/RadioMenu.cs
+++ code/ui/player/rolemenu/radio/RadioMenu.cs
@@ -7,8 +7,27 @@ namespace TTT.UI;
 
 public partial class RadioMenu : Panel
 {
+	private readonly Dictionary<string, List<string>> _sounds = new()
+	{
+		{ "Pistol", new List<string>() { "idk", "wtf" } },
+		{ "Rifle", new List<string>() { "idk", "wtf" } },
+		{ "Sniper", new List<string>() { "idk", "wtf" } },
+		{ "Magnum", new List<string>() { "idk", "wtf" } },
+		{ "Testing", new List<string>() { "idk", "wtf" } },
+		{ "Not Sure", new List<string>() { "idk", "wtf" } },
+		{ "Not Sure 2", new List<string>() { "idk", "wtf" } },
+	};
+
 	public RadioMenu()
 	{
-		StyleSheet.Load( "/ui/player/rolemenu/radiomenu/RadioMenu.scss" );
+		StyleSheet.Load( "/ui/player/rolemenu/radio/RadioMenu.scss" );
+
+		foreach ( var sound in _sounds )
+		{
+			Add.Button( sound.Key, () =>
+			{
+
+			} );
+		}
 	}
 }
