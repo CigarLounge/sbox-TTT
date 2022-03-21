55c6a2efcbb76543d795c726756c9a19529f496a
Matthew Zegar <matthewzegar@gmail.com>
2022-03-20 19:31:59 -0700

    Initial DNA item

---
+++ code/items/equipment/DNAScanner.cs
@@ -0,0 +1,13 @@
+using Sandbox;
+
+namespace TTT;
+
+[Hammer.Skip]
+[Library( "ttt_equipment_dnascanner", Title = "DNA Scanner" )]
+public partial class DNAScanner : Carriable
+{
+	public override void Simulate( Client client )
+	{
+
+	}
+}
