35593450c02ab189eaab0159e1f428214a653e9a
Matthew Zegar <matthewzegar@gmail.com>
2022-03-28 19:01:53 -0700

    Convert perk panel to use HTML

--- code/ui/player/inventorywrapper/InventoryWrapper.cs
+++ code/ui/player/inventorywrapper/InventoryWrapper.cs
@@ -2,13 +2,7 @@ using Sandbox.UI;
 
 namespace TTT.UI;
 
+[UseTemplate]
 public class InventoryWrapper : Panel
 {
-	public InventoryWrapper()
-	{
-		StyleSheet.Load( "/ui/player/inventorywrapper/InventoryWrapper.scss" );
-
-		AddChild<PerkDisplay>();
-		AddChild<InventorySelection>();
-	}
 }
--- code/ui/player/inventorywrapper/perkdisplay/PerkDisplay.cs
+++ code/ui/player/inventorywrapper/perkdisplay/PerkDisplay.cs
@@ -1,18 +1,13 @@
 using System.Collections.Generic;
 using Sandbox;
 using Sandbox.UI;
-using Sandbox.UI.Construct;
 
 namespace TTT.UI;
 
+[UseTemplate]
 public class PerkDisplay : Panel
 {
 	private readonly Dictionary<Perk, PerkSlot> _entries = new();
-
-	public PerkDisplay()
-	{
-		StyleSheet.Load( "/ui/player/inventorywrapper/perkdisplay/PerkDisplay.scss" );
-	}
 
 	public override void Tick()
 	{
@@ -48,35 +43,5 @@ public class PerkDisplay : Panel
 		var perkSlot = new PerkSlot( perk );
 		AddChild( perkSlot );
 		return perkSlot;
-	}
-
-	public class PerkSlot : Panel
-	{
-		private readonly Perk _perk;
-		private readonly Label _name;
-		private readonly Image _image;
-		private readonly Label _activeLabel;
-
-		public PerkSlot( Perk perk )
-		{
-			_perk = perk;
-
-			var panel = Add.Panel( "icon-panel" );
-			_image = panel.Add.Image();
-			_image.SetImage( perk.Info.Icon );
-
-			_activeLabel = panel.Add.Label( "", "active" );
-			_activeLabel.AddClass( "text-shadow" );
-			_activeLabel.AddClass( "centered" );
-
-			_name = Add.Label( perk.Info.Title, "name" );
-			_name.AddClass( "text-shadow" );
-		}
-
-		public override void Tick()
-		{
-			base.Tick();
-			_activeLabel.Text = _perk.SlotText;
-		}
 	}
 }
