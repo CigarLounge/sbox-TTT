b9f4044e2142d649217d1d96fff8f3a040106474
Matthew Zegar <matthewzegar@gmail.com>
2022-03-28 20:34:45 -0700

    use HTML for dna menu

--- code/ui/player/rolemenu/dna/DNAMenu.cs
+++ code/ui/player/rolemenu/dna/DNAMenu.cs
@@ -6,39 +6,20 @@ using System.Linq;
 
 namespace TTT.UI;
 
+[UseTemplate]
 public partial class DNAMenu : Panel
 {
 	private readonly Dictionary<DNA, Sample> _entries = new();
 
-	private readonly Panel _sampleContainer;
-	private Panel _emptyPanel;
-	private readonly Panel _infoContainer;
-	private readonly Button _button;
-	private readonly Checkbox _checkbox;
-
-	public DNAMenu()
-	{
-		StyleSheet.Load( "/ui/player/rolemenu/dna/DNAMenu.scss" );
-
-		var wrapper = Add.Panel();
-
-		_sampleContainer = wrapper.Add.Panel( "sample-container" );
-		_infoContainer = wrapper.Add.Panel( "info-container" );
-
-		_infoContainer.AddClass( "rounded" );
-		_infoContainer.AddClass( "background-color-primary" );
-		var currentCharge = _infoContainer.Add.Label( "95.45%", "charge" );
-		var status = _infoContainer.Add.Label( "CHARGING", "charge-status" );
-		_button = _infoContainer.Add.ButtonWithIcon( "Scan", "radar", "scan-button" );
-
-		_checkbox = new Checkbox();
-		_checkbox.LabelText = "Auto-repeat";
-		_infoContainer.AddChild( _checkbox );
-	}
+	private Panel SampleContainer { get; set; }
+	private Label Charge { get; set; }
+	private Label ChargeStatus { get; set; }
+	private Button ScanButton { get; set; }
+	private Checkbox AutoRepeat { get; set; }
 
 	public override void Tick()
 	{
-		_button.SetClass( "inactive", _checkbox.Checked );
+		ScanButton.SetClass( "inactive", AutoRepeat.Checked );
 
 		if ( Local.Pawn is not Player player )
 			return;
@@ -65,7 +46,7 @@ public partial class DNAMenu : Panel
 	private Sample AddSample( DNA dna )
 	{
 		var panel = new Sample( dna );
-		_sampleContainer.AddChild( panel );
+		SampleContainer.AddChild( panel );
 		return panel;
 	}
 
