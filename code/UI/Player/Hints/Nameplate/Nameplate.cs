3532169303880b3fd42f9eed879de5c1a70eceed
Matthew Zegar <matthewzegar@gmail.com>
2022-03-31 18:03:49 -0700

    nameplate to html

--- code/UI/Player/Hints/Nameplate/Nameplate.cs
+++ code/UI/Player/Hints/Nameplate/Nameplate.cs
@@ -1,62 +1,37 @@
 using Sandbox;
 using Sandbox.UI;
-using Sandbox.UI.Construct;
 
 namespace TTT.UI;
 
+[UseTemplate]
 public class Nameplate : EntityHintPanel
 {
 	public readonly Player _player;
 
-	private readonly Panel _labelHolder;
-	private readonly Label _nameLabel;
-	private readonly Label _roleLabel;
-	private readonly Label _damageIndicatorLabel;
-
+	private Label Name { get; set; }
+	private Label HealthIndicator { get; set; }
+	private Label Role { get; set; }
 	public Nameplate( Player player )
 	{
 		_player = player;
-
-		StyleSheet.Load( "/ui/player/hints/nameplate/Nameplate.scss" );
-
-		_labelHolder = Add.Panel( "label-holder" );
-
-		_nameLabel = _labelHolder.Add.Label( "", "name" );
-		_nameLabel.AddClass( "text-shadow" );
-		_damageIndicatorLabel = _labelHolder.Add.Label( "", "damage-indicator" );
-		_damageIndicatorLabel.AddClass( "text-shadow" );
-		_roleLabel = _labelHolder.Add.Label( "", "role" );
-		_roleLabel.AddClass( "text-shadow-light" );
-
-		this.Enabled( false );
 	}
 
 	public override void Tick()
 	{
-		SetClass( "disabled", !this.IsEnabled() );
-
-		if ( !this.IsEnabled() || !_player.IsValid() )
+		if ( !_player.IsValid() )
 			return;
 
-		// Network sync workaround
-		if ( _player.Health == 0 && _player.IsAlive() )
-		{
-			_damageIndicatorLabel.Text = "";
-		}
-		else
-		{
-			var health = _player.Health / _player.MaxHealth * 100;
-			var healthGroup = _player.GetHealthGroup( health );
+		var health = _player.Health / _player.MaxHealth * 100;
+		var healthGroup = _player.GetHealthGroup( health );
 
-			_damageIndicatorLabel.Style.FontColor = healthGroup.Color;
-			_damageIndicatorLabel.Text = healthGroup.Title;
-		}
+		HealthIndicator.Style.FontColor = healthGroup.Color;
+		HealthIndicator.Text = healthGroup.Title;
 
-		_nameLabel.Text = _player.Client?.Name ?? "";
+		Name.Text = _player.Client?.Name ?? "";
 		if ( _player.Role is not NoneRole && _player.Role is not InnocentRole )
 		{
-			_roleLabel.Text = _player.Role.Title;
-			_roleLabel.Style.FontColor = _player.Role.Color;
+			Role.Text = _player.Role.Title;
+			Role.Style.FontColor = _player.Role.Color;
 		}
 	}
 }
