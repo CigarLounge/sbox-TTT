74cb72912530a73fbf89a867d2d09cb33c5e5c94
Matthew Zegar <matthewzegar@gmail.com>
2022-04-02 09:03:21 -0700

    Increase ammo count for rifles

--- code/Player/Player.Ammo.cs
+++ code/Player/Player.Ammo.cs
@@ -9,7 +9,7 @@ public partial class Player
 	[Net]
 	public List<int> Ammo { get; set; } = new();
 
-	public readonly int[] AmmoCap = new int[] { 0, 60, 16, 20, 12, 40 };
+	public readonly int[] AmmoCap = new int[] { 0, 60, 16, 20, 12, 60 };
 
 	public void ClearAmmo()
 	{
