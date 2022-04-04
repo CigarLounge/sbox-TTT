6caf26f81d543e993b22d3a181e1fc5f87a77fe0
Matthew Zegar <matthewzegar@gmail.com>
2022-04-03 22:35:30 -0700

    Basic stub out

--- code/Items/Equipment/NewtonLauncher.cs
+++ code/Items/Equipment/NewtonLauncher.cs
@@ -8,6 +8,7 @@ public partial class NewtonLauncher : Weapon
 {
 	[Net, Local, Predicted]
 	private int Charge { get; set; }
+	private const int MAX_CHARGE = 100;
 
 	public override string SlotText => $"{Charge}%";
 
@@ -17,5 +18,64 @@ public partial class NewtonLauncher : Weapon
 
 		// While we have no viewmodel let's just show the world model.
 		EnableHideInFirstPerson = false;
+	}
+
+	public override void Simulate( Client client )
+	{
+		if ( TimeSinceDeployed < Info.DeployTime )
+			return;
+
+		if ( Charge < MAX_CHARGE )
+			Charge += 1;
+
+		if ( Charge >= MAX_CHARGE )
+		{
+			using ( LagCompensation() )
+			{
+				Fire();
+			}
+		}
+	}
+
+	private void Fire()
+	{
+		Charge = 0;
+
+		var forward = Owner.EyeRotation.Forward;
+		forward = forward.Normal;
+
+		foreach ( var trace in TraceBullet( Owner.EyePosition, Owner.EyePosition + forward * 20000f, 3.0f ) )
+		{
+			var fullEndPosition = trace.EndPosition + trace.Direction * 3.0f;
+
+			if ( !string.IsNullOrEmpty( Info.TracerParticle ) && trace.Distance > 200 )
+			{
+				var tracer = Particles.Create( Info.TracerParticle );
+				tracer?.SetPosition( 0, trace.StartPosition );
+				tracer?.SetPosition( 1, trace.EndPosition );
+			}
+
+			if ( !IsServer )
+				continue;
+
+			if ( !trace.Entity.IsValid() )
+				continue;
+
+			using ( Prediction.Off() )
+			{
+				trace.Entity.ApplyAbsoluteImpulse( 1000f * fullEndPosition );
+
+				var damageInfo = DamageInfo.FromBullet( trace.EndPosition, forward * 1000f, Info.Damage )
+					.UsingTraceResult( trace )
+					.WithAttacker( Owner )
+					.WithWeapon( this );
+
+				if ( trace.Entity is Player player )
+					player.LastDistanceToAttacker = Owner.Position.Distance( player.Position ).SourceUnitsToMeters();
+
+				OnHit( trace.Entity );
+				trace.Entity.TakeDamage( damageInfo );
+			}
+		}
 	}
 }
