de7b49157d1e1956ca84674461691215d3778a55
Matthew Zegar <matthewzegar@gmail.com>
2022-04-15 21:18:51 -0700

    Dont print deaths to console, remove unused code?

--- code/Game.cs
+++ code/Game.cs
@@ -63,6 +63,11 @@ public partial class Game : Sandbox.Game
 		// Do nothing. The player can't noclip in this mode.
 	}
 
+	public override void OnKilled( Entity pawn )
+	{
+		// Do nothing. Base implementation just adds to a kill feed and prints to console.
+	}
+
 	public override void ClientJoined( Client client )
 	{
 		var player = new Player();
@@ -127,55 +132,5 @@ public partial class Game : Sandbox.Game
 		_lastRound.Start();
 
 		Event.Run( TTTEvent.Game.RoundChanged, oldRound, newRound );
-	}
-
-	public static void Explosion( Entity source, Entity owner, Vector3 position, float radius, float damage, float forceScale )
-	{
-		// Effects
-		Sound.FromWorld( "rust_pumpshotgun.shootdouble", position );
-		Particles.Create( "particles/explosion/barrel_explosion/explosion_barrel.vpcf", position );
-
-		// Damage, etc
-		var overlaps = Entity.FindInSphere( position, radius );
-
-		foreach ( var overlap in overlaps )
-		{
-			if ( overlap is not ModelEntity entity || !entity.IsValid() )
-				continue;
-
-			if ( !entity.IsAlive() )
-				continue;
-
-			if ( !entity.PhysicsBody.IsValid() )
-				continue;
-
-			if ( entity.IsWorld )
-				continue;
-
-			var targetPos = entity.PhysicsBody.MassCenter;
-
-			var dist = Vector3.DistanceBetween( position, targetPos );
-			if ( dist > radius )
-				continue;
-
-			var trace = Trace.Ray( position, targetPos )
-				.Ignore( source )
-				.WorldOnly()
-				.Run();
-
-			if ( trace.Fraction < 0.98f )
-				continue;
-
-			float distanceMul = 1.0f - Math.Clamp( dist / radius, 0.0f, 1.0f );
-			float dmg = damage * distanceMul;
-			float force = (forceScale * distanceMul) * entity.PhysicsBody.Mass;
-			var forceDir = (targetPos - position).Normal;
-
-			var damageInfo = DamageInfo.Explosion( position, forceDir * force, dmg )
-				.WithWeapon( source )
-				.WithAttacker( owner );
-
-			entity.TakeDamage( damageInfo );
-		}
 	}
 }
