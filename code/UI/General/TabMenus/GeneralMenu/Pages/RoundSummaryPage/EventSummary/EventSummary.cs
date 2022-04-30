1a11f61eab0b382afdea7d50f57db1b906658190
Matthew Zegar <matthewzegar@gmail.com>
2022-04-29 18:27:25 -0700

    Event summary stub

---
+++ code/UI/General/TabMenus/GeneralMenu/Pages/RoundSummaryPage/EventSummary/EventSummary.cs
@@ -0,0 +1,52 @@
+using Sandbox.UI;
+using Sandbox.UI.Construct;
+
+namespace TTT.UI;
+
+[UseTemplate]
+public partial class EventSummary : Panel
+{
+	public static EventSummary Instance;
+
+	private Panel Events { get; init; }
+
+	public EventSummary()
+	{
+		Instance = this;
+		Init();
+	}
+
+	public void Init()
+	{
+		Events?.DeleteChildren();
+
+		AddEvent( "flag", 523, "The round has started!" );
+		AddEvent( "flag", 523, "The round has started!" );
+		AddEvent( "flag", 523, "The round has started!" );
+		AddEvent( "flag", 523, "The round has started!" );
+		AddEvent( "flag", 523, "The round has started!" );
+		AddEvent( "flag", 523, "The round has started!" );
+		AddEvent( "flag", 523, "The round has started!" );
+		AddEvent( "flag", 523, "The round has started!" );
+		AddEvent( "flag", 523, "The round has started!" );
+		AddEvent( "flag", 523, "The round has started!" );
+		AddEvent( "flag", 523, "The round has started!" );
+		AddEvent( "flag", 523, "The round has started!" );
+		AddEvent( "flag", 523, "The round has started!" );
+	}
+
+	// TODO: Add proper hookups to event data...
+	private void AddEvent( string eventType, float time, string description )
+	{
+		var container = Events.Add.Panel( "event" );
+		container.Add.Label( GetIcon( eventType ), "icon" );
+		container.Add.Label( time.TimerString(), "time" );
+		container.Add.Label( description, "desc" );
+	}
+
+	// TODO: Proper switch statement.
+	private string GetIcon( string eventType )
+	{
+		return eventType;
+	}
+}
