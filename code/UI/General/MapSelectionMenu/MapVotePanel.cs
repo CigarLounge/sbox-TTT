fa2346a78b1aefffbeaa6c027015151158f48515
Kole <novoselkosta@gmail.com>
2022-06-20 01:48:03 +0200

    Delete all unneeded UI elements at the start of map select

--- code/UI/General/MapSelectionMenu/MapVotePanel.cs
+++ code/UI/General/MapSelectionMenu/MapVotePanel.cs
@@ -9,12 +9,23 @@ namespace TTT.UI;
 [UseTemplate]
 public partial class MapVotePanel : Panel
 {
-	private Label TimeText { get; set; }
-	private Panel Body { get; set; }
-	private readonly List<MapIcon> MapIcons = new();
+	private Label TimeText { get; init; }
+	private Panel Body { get; init; }
+
+	private readonly List<MapIcon> _mapIcons = new();
 
 	public MapVotePanel()
 	{
+		// Delete unneeded UI elements.
+		foreach ( var panel in Local.Hud.Children.ToList() )
+		{
+			if ( panel is FullScreenHintMenu )
+				continue;
+
+			if ( panel is not ChatBox and not VoiceChatDisplay )
+				panel.Delete( true );
+		}
+
 		_ = PopulateMaps();
 	}
 
@@ -39,7 +50,7 @@ public partial class MapVotePanel : Panel
 
 	private MapIcon AddMap( string fullIdent )
 	{
-		var icon = MapIcons.FirstOrDefault( x => x.Ident == fullIdent );
+		var icon = _mapIcons.FirstOrDefault( x => x.Ident == fullIdent );
 
 		if ( icon is not null )
 			return icon;
@@ -48,7 +59,7 @@ public partial class MapVotePanel : Panel
 		icon.AddEventListener( "onclick", () => MapSelectionState.SetVote( fullIdent ) );
 		Body.AddChild( icon );
 
-		MapIcons.Add( icon );
+		_mapIcons.Add( icon );
 		return icon;
 	}
 
@@ -58,7 +69,7 @@ public partial class MapVotePanel : Panel
 
 		TimeText.Text = mapSelectionState.TimeLeftFormatted;
 
-		foreach ( var icon in MapIcons )
+		foreach ( var icon in _mapIcons )
 			icon.VoteCount = "0";
 
 		foreach ( var group in mapSelectionState.Votes.GroupBy( x => x.Value ).OrderByDescending( x => x.Count() ) )
