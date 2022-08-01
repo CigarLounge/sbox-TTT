9eff5cab293364534be0dc69aefd2899c1c116ab
nywek <nywekk@gmail.com>
2022-08-01 10:43:53 +0200

    Add To() extensions for List<Client> & IEnumerable<Client>

---
+++ code/Util/ToExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using Sandbox;
+
+namespace TTT;
+
+public static class ToExtensions
+{
+	public static To To( this List<Client> self )
+	{
+		return Sandbox.To.Multiple( self );
+	}
+
+	public static To To( this IEnumerable<Client> self )
+	{
+		return Sandbox.To.Multiple( self );
+	}
+}
