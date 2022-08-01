11200cb14660134228ce2bec3c48be464e1aa6e5
nywek <nywekk@gmail.com>
2022-08-01 10:43:01 +0200

    Add PossessionNameplate

---
+++ code/UI/Player/PossessionNameplate/PossessionNameplate.cs
@@ -0,0 +1,47 @@
+﻿using Sandbox;
+using Sandbox.UI;
+
+namespace TTT.UI;
+
+[UseTemplate]
+public class PossessionNameplate : Panel
+{
+	private Label PlayerName { get; set; }
+	private Player _player;
+	private Prop _prop;
+
+	public PossessionNameplate( Player player, Prop prop )
+	{
+		_player = player;
+		_prop = prop;
+		PlayerName.SetText( player.SteamName );
+		Local.Hud.AddChild( this );
+	}
+
+	public override void Tick()
+	{
+		if ( _player is null || _prop is null )
+		{
+			Delete();
+			return;
+		}
+
+		var occluded = Trace.Ray( CurrentView.Position, _prop.WorldSpaceBounds.Center )
+			.WorldAndEntities()
+			.Ignore( _prop )
+			.Run()
+			.Hit;
+
+		if ( occluded )
+		{
+			Style.Opacity = 0f;
+			return;
+		}
+
+		var screenPos = _prop.WorldSpaceBounds.Center.ToScreen();
+
+		Style.Opacity = 1;
+		Style.Left = Length.Fraction( screenPos.x );
+		Style.Top = Length.Fraction( screenPos.y );
+	}
+}
