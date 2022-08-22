6231b7593dbf64809b0f97a89f432cea423607a0
mclarty3 <mclarty3@gmail.com>
2022-08-21 23:51:24 -0400

    Implement selecting tag; add rough colours

--- code/UI/General/TabMenus/Scoreboard/Entry/ScoreboardEntry.cs
+++ code/UI/General/TabMenus/Scoreboard/Entry/ScoreboardEntry.cs
@@ -1,5 +1,7 @@
+using System.Collections.Generic;
 using Sandbox;
 using Sandbox.UI;
+using Sandbox.UI.Construct;
 using System;
 
 namespace TTT.UI;
@@ -8,7 +10,6 @@ namespace TTT.UI;
 public class ScoreboardEntry : Panel
 {
 	public PlayerStatus PlayerStatus;
-	public bool Expanded = false;
 	private readonly Client _client;
 
 	private Image PlayerAvatar { get; init; }
@@ -18,11 +19,33 @@ public class ScoreboardEntry : Panel
 	private Label Ping { get; init; }
 	private Label Tag { get; init; }
 
-	private ScoreboardEntryTagger EntryTagger { get; set; }
+	private bool Expanded = false;
+	// private Button FriendButton { get; init; }
+	// private Button SuspectButton { get; init; }
+	// private Button AvoidButton { get; init; }
+	// private Button KillButton { get; init; }
+	// private Button MissingButton { get; init; }
+
+	// private List<Button> Buttons;
+	// private List<string> _tags = new() { "Friend", "Suspect", "Avoid", "Kill", "Missing" };
+	// private List<string> _tags;
+
+
+	private void SetTag(string tag)
+	{
+		Tag.Text = tag;
+		Tag.AddClass(tag);
+	}
 
 	public ScoreboardEntry( Panel parent, Client client ) : base( parent )
 	{
 		_client = client;
+		GetChild(1).Add.Button( "Friend", () =>  { SetTag( "Friend" ); } );
+		GetChild(1).Add.Button( "Suspect", () => { SetTag( "Suspect" ); } );
+		GetChild(1).Add.Button( "Avoid", () =>   { SetTag( "Avoid" ); } );
+		GetChild(1).Add.Button( "Kill", () =>    { SetTag( "Kill" ); } );
+		GetChild(1).Add.Button( "Missing", () => { SetTag( "Missing" ); } );
+		Karma.SetProperty("color", "red");
 	}
 
 	public void Update()
@@ -39,7 +62,6 @@ public class ScoreboardEntry : Panel
 
 		Ping.Text = _client.IsBot ? "BOT" : _client.Ping.ToString();
 		Score.Text = player.Score.ToString();
-		Tag.Text = "Test";
 
 		SetClass( "me", _client == Local.Client );
 
@@ -49,6 +71,8 @@ public class ScoreboardEntry : Panel
 			Style.BackgroundColor = null;
 
 		PlayerAvatar.SetTexture( $"avatar:{_client.PlayerId}" );
+
+		Log.Info(GetChild(1));
 	}
 
 	public void OnClick()
@@ -60,15 +84,12 @@ public class ScoreboardEntry : Panel
 	{
 		if (!Expanded) {
 			Style.Set("height", "76px");
-			EntryTagger = new ScoreboardEntryTagger(this, _client);
-
-			// AddChild(EntryTagger);
-			Parent.AddChild(EntryTagger);
+			GetChild(1).RemoveClass("no-display");
 			Expanded = true;
 		}
 		else {
 			Style.Set("height", "38px");
-			EntryTagger.Delete(true);
+			GetChild(1).AddClass("no-display");
 			Expanded = false;
 		}
 	}
