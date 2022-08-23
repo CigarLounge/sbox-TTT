653e9b052981b3666e104eb0e8c2950bb05a7228
Matthew Zegar <matthewzegar@gmail.com>
2022-08-22 22:18:09 -0700

    Fea: Add voice chat icon above player

--- code/UI/General/VoiceChat/VoiceChatEntry.cs
+++ code/UI/General/VoiceChat/VoiceChatEntry.cs
@@ -2,6 +2,7 @@ using System;
 
 using Sandbox;
 using Sandbox.UI;
+using Sandbox.UI.Construct;
 
 namespace TTT.UI;
 
@@ -13,7 +14,9 @@ public class VoiceChatEntry : Panel
 	private Label Name { get; init; }
 	private Image Avatar { get; init; }
 
+	private readonly WorldPanel _worldPanel;
 	private readonly Client _client;
+	private readonly Player _player;
 	private float _voiceLevel = 0.5f;
 	private float _targetVoiceLevel = 0;
 	private readonly float _voiceTimeout = 0.1f;
@@ -25,10 +28,16 @@ public class VoiceChatEntry : Panel
 		Parent = parent;
 
 		_client = client;
+		_player = client.Pawn as Player;
 		Friend = new( client.PlayerId );
 
 		Avatar.SetTexture( $"avatar:{client.PlayerId}" );
 		Name.Text = Friend.Name;
+
+		_worldPanel = new WorldPanel();
+		_worldPanel.StyleSheet.Load( "/UI/General/VoiceChat/VoiceChatEntry.scss" );
+		_worldPanel.Add.Image( classname: "voice-icon" ).SetTexture( "ui/voicechat.png" );
+		_worldPanel.SceneObject.Flags.ViewModelLayer = true;
 	}
 
 	public void Update( float level )
@@ -52,9 +61,21 @@ public class VoiceChatEntry : Panel
 		if ( timeoutInv <= 0 )
 		{
 			Delete();
+			_worldPanel?.Delete();
 			return;
 		}
 
 		_voiceLevel = _voiceLevel.LerpTo( _targetVoiceLevel, Time.Delta * 40.0f );
+
+		if ( !_player.IsAlive() )
+		{
+			_worldPanel?.Delete();
+			return;
+		}
+
+		var tx = _player.GetBoneTransform( "head" );
+		tx.Position += Vector3.Up * 18f + (Vector3.Up * _voiceLevel);
+		tx.Rotation = CurrentView.Rotation.RotateAroundAxis( Vector3.Up, 180f );
+		_worldPanel.Transform = tx;
 	}
 }
