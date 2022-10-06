0c356cdd03915f964771028949a0f211ea4a8846
Matthew Zegar <matthewzegar@gmail.com>
2022-10-05 19:05:59 -0700

    Remove Hud Init()

--- code/UI/Hud.cs
+++ code/UI/Hud.cs
@@ -9,34 +9,5 @@ public class Hud : RootPanel
 	public Hud()
 	{
 		Local.Hud = this;
-		Init();
-	}
-
-	private void Init()
-	{
-		AddChild<HintDisplay>();
-		AddChild<PlayerInfo>();
-		AddChild<InventoryWrapper>();
-		AddChild<ChatBox>();
-		AddChild<QuickChat>();
-		AddChild<VoiceChatDisplay>();
-		AddChild<RoundTimer>();
-		AddChild<VoiceList>();
-		AddChild<InfoFeed>();
-		AddChild<FullScreenHintMenu>();
-		AddChild<TabMenus>();
-		AddChild<Crosshair>();
-		AddChild<CarriableHint>();
-		AddChild<RoleMenu>();
-		AddChild<DamageIndicator>();
-		AddChild<WorldPoints>();
-		AddChild<SpectatingHint>();
-	}
-
-	[Event.Hotload]
-	private void OnHotReload()
-	{
-		DeleteChildren( true );
-		Init();
 	}
 }
