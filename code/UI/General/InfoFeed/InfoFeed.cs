878940ef9de50db8a07c01c1cbdad8578994660a
Matthew Zegar <matthewzegar@gmail.com>
2022-12-15 15:11:55 -0800

    Converted infofeed to razor

--- code/UI/General/InfoFeed/InfoFeed.cs
+++ code/UI/General/InfoFeed/InfoFeed.cs
@@ -1,69 +1,80 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using Sandbox;
 using Sandbox.UI;
-using System;
 
 namespace TTT.UI;
 
-[UseTemplate]
 public partial class InfoFeed : Panel
 {
 	public static InfoFeed Instance { get; private set; }
 
 	public InfoFeed() => Instance = this;
 
-	[ClientRpc]
-	public static void AddEntry( string method )
+	private const int MaxMessagesDisplayed = 5;
+	private const float DisplayDuration = 5f;
+	private readonly Queue<InfoFeedEntry> _entryQueue = new();
+	private TimeUntil _timeUntilNextDisplay = 0f;
+	private TimeSince _timeSinceLastDisplayed = 0f;
+	private TimeUntil _timeUntilNextDelete = 0f;
+
+	public void AddToFeed( InfoFeedEntry entry )
 	{
-		Instance.AddChild<InfoFeedEntry>().AddLabel( method, "method" );
+		_entryQueue.Enqueue( entry );
+	}
+
+	public override void Tick()
+	{
+		if ( _entryQueue.Any() && _timeUntilNextDisplay && ChildrenCount < MaxMessagesDisplayed )
+		{
+			var newEntry = _entryQueue.Dequeue();
+			Instance.AddChild( newEntry );
+
+			_timeUntilNextDisplay = 1f;
+			_timeSinceLastDisplayed = 0f;
+		}
+
+		if ( Children.Any() && _timeUntilNextDelete && _timeSinceLastDisplayed > DisplayDuration )
+		{
+			var oldEntry = Children.ElementAt( 0 );
+			oldEntry.Delete();
+			_timeUntilNextDelete = DisplayDuration;
+		}
 	}
 
 	[ClientRpc]
-	public static void AddEntry( string method, Color color )
+	public static void AddEntry( string message )
 	{
-		Instance.AddChild<InfoFeedEntry>().AddLabel( method, "method" ).Style.FontColor = color;
+		Instance.AddToFeed( new InfoFeedEntry( message ) );
+	}
+
+	[ClientRpc]
+	public static void AddEntry( string message, Color color )
+	{
+		Instance.AddToFeed( new InfoFeedEntry( message, color ) );
 	}
 
 	[ClientRpc]
 	public static void AddEntry( Player player, string message )
 	{
-		var entry = Instance.AddChild<InfoFeedEntry>();
-
-		var leftLabel = entry.AddLabel( player.IsLocalPawn ? "You" : player.SteamName, "left" );
-		leftLabel.Style.FontColor = !player.IsRoleKnown ? Color.White : player.Role.Color;
-
-		entry.AddLabel( message, "method" );
+		Instance.AddToFeed( new InfoFeedEntry( player, message ) );
 	}
 
 	[ClientRpc]
-	public static void AddRoleEntry( RoleInfo roleInfo, string interaction )
+	public static void AddRoleEntry( RoleInfo roleInfo, string message )
 	{
-		var entry = Instance.AddChild<InfoFeedEntry>();
-
-		var leftLabel = entry.AddLabel( $"{roleInfo.Title}s", "left" );
-		leftLabel.Style.FontColor = roleInfo.Color;
-
-		entry.AddLabel( interaction, "method" );
+		Instance.AddToFeed( new InfoFeedEntry( roleInfo, message ) );
 	}
 
 	[ClientRpc]
-	public static void AddPlayerToPlayerEntry( Player left, Player right, string method, string suffix = "" )
+	public static void AddPlayerToPlayerEntry( Player left, Player right, string message, string suffix = "" )
 	{
-		var entry = Instance.AddChild<InfoFeedEntry>();
-
-		var leftLabel = entry.AddLabel( left.IsLocalPawn ? "You" : left.SteamName, "left" );
-		leftLabel.Style.FontColor = !left.IsRoleKnown ? Color.White : left.Role.Color;
-
-		entry.AddLabel( method, "method" );
-
-		var rightLabel = entry.AddLabel( right.IsLocalPawn ? "You" : right.SteamName, "right" );
-		rightLabel.Style.FontColor = !right.IsRoleKnown ? Color.White : right.Role.Color;
-
-		if ( !suffix.IsNullOrEmpty() )
-			entry.AddLabel( suffix, "append" );
+		Instance.AddToFeed( new InfoFeedEntry( left, right, message, suffix ) );
 	}
 
 	[GameEvent.Player.CorpseFound]
-	private void OnCorpseFound( Player player )
+	private static void OnCorpseFound( Player player )
 	{
 		AddPlayerToPlayerEntry
 		(
@@ -93,18 +104,12 @@ public partial class InfoFeed : Panel
 
 		var karma = MathF.Round( player.BaseKarma );
 		var df = MathF.Round( 100f - player.DamageFactor * 100f );
-
-		string text;
-		if ( df == 0 )
-			text = $"Your karma is {karma}, you'll deal full damage this round.";
-		else
-			text = $"Your karma is {karma}, you'll deal {df}% reduced damage this round.";
-
-		AddEntry( text );
+		var damageFactor = df == 0 ? $"Your karma is {karma}, you'll deal full damage this round." : $"Your karma is {karma}, you'll deal {df}% reduced damage this round.";
+		AddEntry( damageFactor );
 	}
 
 	[GameEvent.Round.End]
-	private void OnRoundEnd( Team winningTeam, WinType winType )
+	private void OnRoundEnd( Team _, WinType _1 )
 	{
 		this.Enabled( false );
 		DeleteChildren();
--- code/UI/General/InfoFeed/InfoFeedEntry.cs
+++ code/UI/General/InfoFeed/InfoFeedEntry.cs
@@ -1,25 +1,39 @@
-using Sandbox;
 using Sandbox.UI;
 using Sandbox.UI.Construct;
 
 namespace TTT.UI;
 
-[UseTemplate]
 public partial class InfoFeedEntry : Panel
 {
-	private readonly TimeUntil _timeUntilDelete = 6;
-
-	public Label AddLabel( string text, string classname )
+	public InfoFeedEntry( string message )
 	{
-		var label = Add.Label( text, classname );
-		return label;
+		Add.Label( message );
 	}
 
-	public override void Tick()
+	public InfoFeedEntry( string message, Color color )
 	{
-		base.Tick();
+		Add.Label( message ).Style.FontColor = color;
+	}
 
-		if ( _timeUntilDelete )
-			Delete();
+	public InfoFeedEntry( Player player, string message )
+	{
+		Add.Label( player.IsLocalPawn ? "You" : player.SteamName, "entry" ).Style.FontColor = !player.IsRoleKnown ? Color.White : player.Role.Color;
+		Add.Label( message );
+	}
+
+	public InfoFeedEntry( RoleInfo roleInfo, string message )
+	{
+		Add.Label( $"{roleInfo.Title}s", "entry" ).Style.FontColor = roleInfo.Color;
+		Add.Label( message );
+	}
+
+	public InfoFeedEntry( Player left, Player right, string message, string suffix = "" )
+	{
+		Add.Label( left.IsLocalPawn ? "You" : left.SteamName, "entry" ).Style.FontColor = !left.IsRoleKnown ? Color.White : left.Role.Color;
+		Add.Label( message );
+		Add.Label( right.IsLocalPawn ? "You" : right.SteamName, "entry" ).Style.FontColor = !right.IsRoleKnown ? Color.White : right.Role.Color;
+
+		if ( !suffix.IsNullOrEmpty() )
+			Add.Label( suffix );
 	}
 }
