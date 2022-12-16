0a98543b989d7c19af6afdfb016f32e811d646cc
Matthew Zegar <matthewzegar@gmail.com>
2022-12-16 15:24:43 -0800

    Nameplate hint to razor

--- code/UI/Player/Hints/Nameplate/Nameplate.cs
+++ code/UI/Player/Hints/Nameplate/Nameplate.cs
@@ -1,18 +1,15 @@
+using System;
 using Sandbox;
 using Sandbox.UI;
 
 namespace TTT.UI;
 
-[UseTemplate]
-public class Nameplate : Panel
+public partial class Nameplate : Panel
 {
 	public readonly Player _player;
 
-	private Label Name { get; init; }
-	private Label HealthStatus { get; init; }
-	private Label KarmaStatus { get; init; }
-	private Label Role { get; init; }
-	private Label Tag { get; init; }
+	private ColorGroup _healthGroup { get; set; }
+	private ColorGroup _karmaGroup { get; set; }
 
 	public Nameplate( Player player ) => _player = player;
 
@@ -21,30 +18,9 @@ public class Nameplate : Panel
 		if ( !_player.IsValid() )
 			return;
 
-		var health = _player.Health / Player.MaxHealth * 100;
-		var healthGroup = _player.GetHealthGroup( health );
-
-		HealthStatus.Style.FontColor = healthGroup.Color;
-		HealthStatus.Text = healthGroup.Title;
-
-		var karmaGroup = Karma.GetKarmaGroup( _player );
-
-		KarmaStatus.Style.FontColor = karmaGroup.Color;
-		KarmaStatus.Text = karmaGroup.Title;
-
-		Name.Text = _player.Client?.Name ?? "";
-
-		if ( _player.Role is not NoneRole and not Innocent )
-		{
-			Role.Text = _player.Role.Title;
-			Role.Style.FontColor = _player.Role.Color;
-		}
-		else
-		{
-			Role.Enabled( false );
-		}
-
-		Tag.Text = _player.TagGroup.Title;
-		Tag.Style.FontColor = _player.TagGroup.Color;
+		_healthGroup = _player.GetHealthGroup( _player.Health / Player.MaxHealth * 100 );
+		_karmaGroup = Karma.GetKarmaGroup( _player );
 	}
+
+	protected override int BuildHash() => !_player.IsValid() ? -1 : HashCode.Combine( _player.Role, _player.TagGroup, _healthGroup, _karmaGroup );
 }
