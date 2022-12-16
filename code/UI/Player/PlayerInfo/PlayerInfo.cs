568c25b968caed9fd0e5396f830d576a72436308
Matthew Zegar <matthewzegar@gmail.com>
2022-12-15 16:02:38 -0800

    Player info to razor

--- code/UI/Player/PlayerInfo/PlayerInfo.cs
+++ code/UI/Player/PlayerInfo/PlayerInfo.cs
@@ -1,66 +1,42 @@
 using Sandbox;
 using Sandbox.UI;
-using System.Threading.Tasks;
 
 namespace TTT.UI;
 
-[UseTemplate]
-public class PlayerInfo : Panel
+public partial class PlayerInfo : Panel
 {
-	public static PlayerInfo Instance;
-
 	private Panel RoleContainer { get; set; }
 	private Label Role { get; set; }
 
 	private Panel HealthContainer { get; set; }
 	private Label Health { get; set; }
 
-	public PlayerInfo()
-	{
-		Instance = this;
-	}
-
-	[GameEvent.Player.TookDamage]
-	private void OnHit( Player player )
-	{
-		if ( !this.IsEnabled() )
-			return;
-
-		_ = TakeHit();
-	}
-
-
-	private async Task TakeHit()
-	{
-		AddClass( "hit" );
-		await GameTask.Delay( 200 );
-		RemoveClass( "hit" );
-	}
-
 	public override void Tick()
 	{
-		var player = Game.LocalPawn as Player;
-
-		this.Enabled( PlayerCamera.Target.IsAlive() );
+		this.Enabled( PlayerCamera.Target.IsValid() && PlayerCamera.Target.IsAlive() );
 		if ( !this.IsEnabled() )
 			return;
 
-		if ( PlayerCamera.Target.Role is NoneRole )
+		var hasRole = PlayerCamera.Target.Role is not NoneRole;
+		HealthContainer.SetClass( "left-flat", hasRole );
+		RoleContainer.Enabled( hasRole );
+
+		if ( hasRole )
 		{
-			RoleContainer.Enabled( false );
-			HealthContainer.Style.BorderTopLeftRadius = 4;
-			HealthContainer.Style.BorderBottomLeftRadius = 4;
-		}
-		else
-		{
-			RoleContainer.Enabled( true );
 			RoleContainer.Style.BackgroundColor = PlayerCamera.Target.Role.Color;
 			Role.Text = PlayerCamera.Target.Role.Title;
-
-			HealthContainer.Style.BorderTopLeftRadius = 0;
-			HealthContainer.Style.BorderBottomLeftRadius = 0;
 		}
 
 		Health.Text = $"✚ {PlayerCamera.Target.Health.CeilToInt()}";
+	}
+
+	[GameEvent.Player.TookDamage]
+	private void OnHit( Player _ )
+	{
+		if ( !this.IsEnabled() )
+			return;
+
+		HealthContainer.AddClass( "hit" );
+		Utils.DelayAction( 0.2f, () => { HealthContainer.RemoveClass( "hit" ); } );
 	}
 }
