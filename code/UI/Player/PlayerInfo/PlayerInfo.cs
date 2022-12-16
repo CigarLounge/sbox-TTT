adcc43335fa817e5a16c9c97fdcd21dbd4a2e92d
Matthew Zegar <matthewzegar@gmail.com>
2022-12-16 14:34:14 -0800

    PlayerInfo refactored a bit more to use razor

--- code/UI/Player/PlayerInfo/PlayerInfo.cs
+++ code/UI/Player/PlayerInfo/PlayerInfo.cs
@@ -1,34 +1,13 @@
-using Sandbox;
+using System;
 using Sandbox.UI;
 
 namespace TTT.UI;
 
 public partial class PlayerInfo : Panel
 {
-	private Panel RoleContainer { get; set; }
-	private Label Role { get; set; }
-
 	private Panel HealthContainer { get; set; }
-	private Label Health { get; set; }
 
-	public override void Tick()
-	{
-		this.Enabled( PlayerCamera.Target.IsValid() && PlayerCamera.Target.IsAlive() );
-		if ( !this.IsEnabled() )
-			return;
-
-		var hasRole = PlayerCamera.Target.Role is not NoneRole;
-		HealthContainer.SetClass( "left-flat", hasRole );
-		RoleContainer.Enabled( hasRole );
-
-		if ( hasRole )
-		{
-			RoleContainer.Style.BackgroundColor = PlayerCamera.Target.Role.Color;
-			Role.Text = PlayerCamera.Target.Role.Title;
-		}
-
-		Health.Text = $"✚ {PlayerCamera.Target.Health.CeilToInt()}";
-	}
+	protected override int BuildHash() => HashCode.Combine( PlayerCamera.Target.Role, PlayerCamera.Target.Health );
 
 	[GameEvent.Player.TookDamage]
 	private void OnHit( Player _ )
