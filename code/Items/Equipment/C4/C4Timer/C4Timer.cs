3304c71cd521fdbeb12c191e6acf95fc5af5c112
Matthew Zegar <matthewzegar@gmail.com>
2022-12-17 12:23:52 -0800

    C4 timer to razor

--- code/Items/Equipment/C4/C4Timer/C4Timer.cs
+++ code/Items/Equipment/C4/C4Timer/C4Timer.cs
@@ -1,24 +1,18 @@
+using System;
 using Sandbox.UI;
 
 namespace TTT.UI;
 
-[UseTemplate]
-public class C4Timer : WorldPanel
+public partial class C4Timer : WorldPanel
 {
+	private string _timer;
 	private readonly C4Entity _c4;
-	private Label Label { get; init; }
-
 	public C4Timer( C4Entity c4 ) => _c4 = c4;
-
 	public override void Tick()
 	{
-		base.Tick();
-
 		SceneObject.Transform = _c4.GetAttachment( "timer" ) ?? default;
-
-		if ( !_c4.IsArmed )
-			Label.Text = "00:00";
-		else
-			Label.Text = _c4.TimeUntilExplode.Relative.TimerString();
+		_timer = _c4.IsArmed ? _c4.TimeUntilExplode.Relative.TimerString() : "00:00";
 	}
+
+	protected override int BuildHash() => HashCode.Combine( _timer );
 }
