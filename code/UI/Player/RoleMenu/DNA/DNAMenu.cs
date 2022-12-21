f0535c26e1de0e8faf25dbbccfdc8581876e5086
Matthew Zegar <matthewzegar@gmail.com>
2022-12-20 20:50:00 -0800

    DNA menu -> razor

--- code/UI/Player/RoleMenu/DNA/DNAMenu.cs
+++ code/UI/Player/RoleMenu/DNA/DNAMenu.cs
@@ -1,22 +1,16 @@
+using System;
 using System.Collections.Generic;
 using Sandbox;
 using Sandbox.UI;
-using Sandbox.UI.Construct;
-using System.Linq;
 
 namespace TTT.UI;
 
-[UseTemplate]
 public partial class DNAMenu : Panel
 {
 	private readonly Dictionary<DNA, DNASample> _entries = new();
 	private DNAScanner _dnaScanner;
 
-	private Panel SampleContainer { get; init; }
-	private Panel Empty { get; init; }
-	private Label Charge { get; init; }
-	private Label ChargeStatus { get; init; }
-	private Checkbox AutoScan { get; init; }
+	private bool AutoScan { get; set; } = false;
 
 	public override void Tick()
 	{
@@ -27,95 +21,8 @@ public partial class DNAMenu : Panel
 		if ( !_dnaScanner.IsValid() )
 			return;
 
-		ChargeStatus.Text = _dnaScanner.IsCharging ? "CHARGING" : "READY";
-
-		if ( _dnaScanner.AutoScan != AutoScan.Checked )
-			SetAutoScan( AutoScan.Checked );
-
-		Charge.Text = _dnaScanner.SlotText;
-
-		foreach ( var dna in _dnaScanner.DNACollected )
-		{
-			if ( !_entries.ContainsKey( dna ) )
-				_entries[dna] = AddDNASample( dna );
-		}
-
-		foreach ( var dnaPanel in _entries.Values )
-		{
-			if ( !_dnaScanner.DNACollected.Contains( dnaPanel.DNA ) )
-			{
-				_entries.Remove( dnaPanel.DNA );
-				dnaPanel?.Delete();
-			}
-
-			dnaPanel.SetClass( "selected", _dnaScanner?.SelectedId == dnaPanel.DNA.Id );
-		}
-
-		Empty.Enabled( !_entries.Any() );
-	}
-
-	private DNASample AddDNASample( DNA dna )
-	{
-		var panel = new DNASample( dna );
-		SampleContainer.AddChild( panel );
-		return panel;
-	}
-
-	public class DNASample : Panel
-	{
-		public DNA DNA { get; private set; }
-
-		public DNASample( DNA dna )
-		{
-			DNA = dna;
-
-			var deleteButton = Add.Icon( "cancel", "delete-button" );
-			deleteButton.AddEventListener( "onclick", () => { DeleteSample( dna.Id ); } );
-
-			Add.Button( $"#{dna.Id} - {dna.SourceName}", () => { SetActiveSample( dna.Id ); } );
-		}
-	}
-
-	[ConCmd.Server]
-	public static void SetActiveSample( int id )
-	{
-		var player = ConsoleSystem.Caller.Pawn as Player;
-		if ( !player.IsValid() )
-			return;
-
-		var scanner = player.Inventory.Find<DNAScanner>();
-		if ( !scanner.IsValid() )
-			return;
-
-		foreach ( var dna in scanner.DNACollected )
-		{
-			if ( dna.Id == id )
-			{
-				scanner.SelectedId = id;
-				return;
-			}
-		}
-	}
-
-	[ConCmd.Server]
-	public static void DeleteSample( int id )
-	{
-		var player = ConsoleSystem.Caller.Pawn as Player;
-		if ( !player.IsValid() )
-			return;
-
-		var scanner = player.Inventory.Find<DNAScanner>();
-		if ( !scanner.IsValid() )
-			return;
-
-		foreach ( var dna in scanner.DNACollected )
-		{
-			if ( dna.Id == id )
-			{
-				scanner.RemoveDNA( dna );
-				return;
-			}
-		}
+		if ( _dnaScanner.AutoScan != AutoScan )
+			SetAutoScan( AutoScan );
 	}
 
 	[ConCmd.Server]
@@ -130,5 +37,10 @@ public partial class DNAMenu : Panel
 			return;
 
 		scanner.AutoScan = enabled;
+	}
+
+	protected override int BuildHash()
+	{
+		return HashCode.Combine( _entries.Count, _dnaScanner?.IsCharging, _dnaScanner?.SlotText );
 	}
 }
