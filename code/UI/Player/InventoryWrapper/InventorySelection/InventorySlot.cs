6d9c6b9fc1bb37e69f73d3da97a93569533ce54a
Matthew Zegar <matthewzegar@gmail.com>
2022-12-21 23:25:39 -0800

    More inventory stuff to razor style

--- code/UI/Player/InventoryWrapper/InventorySelection/InventorySlot.cs
+++ code/UI/Player/InventoryWrapper/InventorySelection/InventorySlot.cs
@@ -6,6 +6,10 @@ namespace TTT.UI;
 public partial class InventorySlot : Panel
 {
 	public Carriable Carriable { get; set; }
-	public Panel SlotNumber { get; set; }
-	protected override int BuildHash() => HashCode.Combine( Carriable.SlotText, CameraMode.Target.Role.Color );
+	public bool IsFirst { get; set; } = false;
+	public bool IsLast { get; set; } = false;
+	protected override int BuildHash()
+	{
+		return HashCode.Combine( Carriable.SlotText, CameraMode.Target.Role.Color, Carriable.IsActiveCarriable, IsFirst, IsLast );
+	}
 }
