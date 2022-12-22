6353b5a0e542024f759a48ff08a98b5e5735132a
Matthew Zegar <matthewzegar@gmail.com>
2022-12-21 23:29:22 -0800

    Radar to use razor style

--- code/UI/Player/InventoryWrapper/PerkDisplay/PerkSlot.cs
+++ code/UI/Player/InventoryWrapper/PerkDisplay/PerkSlot.cs
@@ -5,8 +5,6 @@ namespace TTT.UI;
 
 public partial class PerkSlot : Panel
 {
-	private readonly Perk _perk;
-
-	public PerkSlot( Perk perk ) => _perk = perk;
-	protected override int BuildHash() => HashCode.Combine( _perk.SlotText );
+	public Perk Perk { get; set; }
+	protected override int BuildHash() => HashCode.Combine( Perk.SlotText );
 }
