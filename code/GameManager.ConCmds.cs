c1e53020ac4dbf82e11d4d35b0026ca06ccc99d7
Ephememory <leetvipertm@gmail.com>
2023-01-07 23:43:50 -0600

    Add debug noclip

--- code/GameManager.ConCmds.cs
+++ code/GameManager.ConCmds.cs
@@ -129,4 +129,16 @@ public partial class GameManager
 
 		player.Kill();
 	}
+
+#if DEBUG
+	[ConCmd.Admin( "noclip" )]
+	public static void Noclip()
+	{
+		var player = ConsoleSystem.Caller.Pawn as Player;
+		player.Controller.NoclipEnabled = !player.Controller.NoclipEnabled;
+
+		var status = player.Controller.NoclipEnabled ? "enabled" : "disabled";
+		Log.Info( $"Noclip: {status}" );
+	}
+#endif
 }
