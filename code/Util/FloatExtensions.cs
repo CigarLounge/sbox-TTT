e05c9ef08d56ab3d837c7d8990f4fecbe307f9ce
Matthew Zegar <matthewzegar@gmail.com>
2023-01-11 20:55:43 -0800

    I dont like the default bool

--- code/Util/FloatExtensions.cs
+++ code/Util/FloatExtensions.cs
@@ -13,13 +13,12 @@ public static class FloatExtensions
 	public static float SourceUnitsToMeters( this float sourceUnits ) => sourceUnits / 39.37f;
 
 	/// <summary>
-	/// Returns a float as in timer format
+	/// Formats a float into a timer string
 	/// </summary>
 	/// <param name="seconds">The amount of seconds</param>
-	/// <param name="displayHours">If we should show hours in the formatted timer</param>
-	public static string TimerFormat( this float seconds, bool displayHours = false )
+	public static string TimerFormat( this float seconds )
 	{
-		var timer = TimeSpan.FromSeconds( seconds.CeilToInt() ).ToString( displayHours ? @"hh\:mm\:ss" : @"mm\:ss" );
+		var timer = TimeSpan.FromSeconds( seconds.CeilToInt() ).ToString( @"mm\:ss" );
 		return (int)seconds < 0 ? $"+{timer}" : timer;
 	}
 }
